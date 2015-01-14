e96ee855d86438d900a4321d93cac48b07a11685
Brendan Quinn <bquinn@oneclickventures.com>
2015-01-14 09:54:01 -0500

    added snapscroll and styling to banner carousel

--- brendan.js
+++ brendan.js
@@ -1,5 +1,7 @@
 var timer = 6000;
-cards = [];
+var lastScrollLocation = 0;
+var cards = [];
+var tags = [];
 viewPort = $(window).width();
 function Card(pic, altText, bgColor, html) {
     this.pic = pic;
@@ -67,13 +69,19 @@ $(document).ready(function(){
     function populateCarousel (cards) {
 
         for (var i=0; i < cards.length; i++) {
-            $('#carousel-ul').append('<li class=\'carousel-item\' style=\'background-color: ' + cards[i].bgColor + '\'>' + cards[i].content + '</li>')
+
+            // Populates carousel-ul with li's
+            $('#carousel-ul').append('<li id=\'carousel-item-' + i + '\' style=\'background-color: ' + cards[i].bgColor + '\'><div class=\'item-inner\'>' + cards[i].content + '</div></li>');
+
+            // Setting Dots
             if (i === 0) {
                 $('#dot-inner').append('<div id=\'dot-' + i + '\' class=\'dot white-dot\' data-place=\'' + i + '\'></div>');
             }
             else {
                 $('#dot-inner').append('<div id=\'dot-' + i + '\' class=\'dot\' data-place=\'' + i + '\'></div>');
             }
+            // Array of DOM elements to run .forEach in snapScroll
+            tags.push($('#carousel-item-' + i));
         }
     }
 
@@ -145,7 +153,7 @@ $(document).ready(function(){
         // Get starting left indent
         var startLeftIndent = parseInt($('#carousel-ul').css('left'));
 
-        var itemWidth = $('#carousel-ul li').outerWidth() + 10; // 10 is for compensation for margin
+        var itemWidth = $('#carousel-ul li').outerWidth(); // 10 is for compensation for margin
 
         /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - itemWidth) */
         var leftIndent = startLeftIndent + itemWidth;
@@ -181,7 +189,7 @@ $(document).ready(function(){
         var startLeftIndent = parseInt($('#carousel-ul').css('left'));
 
         // Get the width of the items ( i like making the jquery part dynamic, so if you change the width in the css you won't have o change it here too ) '
-        var itemWidth = $('#carousel-ul li').outerWidth() + 10; // 10 is for compensation for margin
+        var itemWidth = $('#carousel-ul li').outerWidth();
 
         // Calculate the new left indent of the unordered list
         var leftIndent = parseInt($('#carousel-ul').css('left')) - itemWidth;
@@ -250,6 +258,113 @@ $(document).ready(function(){
             return;
         }
     }
-});
+
+/* Snap scroll Event
+////////////////////////////////////////////////////////// */
+
+    // Scroll Event
+    if (viewPort < 910) {
+        $('#carousel-inner').on('scroll', function () {
+
+            determineDirection($(this))
+        });
+    }
+
+    function determineDirection ($target) {
+
+        var currentScrollLocation = $target.scrollLeft();
+
+        if (lastScrollLocation <= currentScrollLocation ) {
+            console.log('left!')
+            snapScrollLeft(currentScrollLocation);
+        }
+        else {
+            console.log('right')
+            snapScrollRight(currentScrollLocation);
+        }
+
+        lastScrollLocation = currentScrollLocation;
+    } // End Determine Direction
+
+    function snapScrollLeft (currentScrollLocation) {
+        var left = currentScrollLocation;
+        console.log(left);
+        tags.forEach(function(x, y) {
+            var itemWidth = x.outerWidth();
+            var adjustedIndex = y + 1;
+            var itemPositionLeft = itemWidth * y;
+            var itemPositionRight = itemWidth * adjustedIndex;
+            var moveTo = itemPositionRight;
+
+            if (left > itemWidth * (tags.length - 1)) {
+                $('#carousel-inner').animate({
+                    scrollLeft: itemWidth * (tags.length - 1) }, 500, function () {
+                    console.log('At End');
+                });
+            }
+            else if (left % itemWidth < 30) {
+                console.log('do nothing')
+                return;
+            }
+            else if (left > itemPositionLeft && left < itemPositionRight && left % 10 === 0) {
+                $('#carousel-inner').off('scroll');
+                console.log('turn off!')
+                $('#carousel-inner').animate({
+                    scrollLeft: moveTo }, 500, function () {
+
+                    console.log('success!!');
+
+                    $('#carousel-inner').on('scroll', function(){
+
+                        determineDirection($(this))
+                    });
+                });
+            }
+        });
+    }
+
+    function snapScrollRight (currentScrollLocaiton) {
+        console.log('he is going backwards')
+        var left = currentScrollLocaiton;
+        console.log(left);
+        tags.forEach(function(x, y) {
+            var itemWidth = x.outerWidth();
+            var adjustedIndex = y + 1;
+            var itemPositionLeft = itemWidth * y;
+            var itemPositionRight = itemWidth * adjustedIndex;
+            var moveTo = itemPositionLeft;
+
+            if (left === 0) {
+                //$('#carousel-inner').animate({
+                //    scrollLeft: itemWidth * (tags.length - 1) }, 500, function () {
+                //    console.log('At Beginning');
+                //});
+                console.log('at beginning')
+            }
+            else if ((left + 40) % itemWidth < 30) {
+                console.log('do nothing going backwards')
+                return;
+            }
+            else if (left > itemPositionLeft && left < itemPositionRight && left % 10 === 0) {
+                $('#carousel-inner').off('scroll');
+                console.log('turn off!')
+                $('#carousel-inner').animate({
+                    scrollLeft: moveTo }, 500, function () {
+
+                    console.log('success!!');
+
+                    $('#carousel-inner').on('scroll', function(){
+
+                        determineDirection($(this))
+                    });
+                });
+            }
+        });
+
+
+    }; // End snapScrollRight
+
+}); // End Doc Ready
+
 
 
